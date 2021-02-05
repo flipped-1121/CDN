25e15cea9c04030adbf1739d69515dc1ca87525d
Kang <61106285+flipped-1121@users.noreply.github.com>
2021-02-05 17:12:58 +0800

    Update cheat.js

--- cheat.js
+++ cheat.js
@@ -2,12 +2,12 @@
  var titleTime;
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
-         $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/flipped-1121/BlogPictures/flipped-1121-PIC/36.png");
+         $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/flipped-1121/BlogPictures/flipped-1121-PIC/36.ico");
          document.title = '(つェ⊂)我藏好了哦~';
          clearTimeout(titleTime);
      } 
      else {
-         $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/flipped-1121/BlogPictures/flipped-1121-PIC/36.png");
+         $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/flipped-1121/BlogPictures/flipped-1121-PIC/36.ico");
          document.title = '(*´∇｀*) 被你发现啦~' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
