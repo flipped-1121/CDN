1a91189ed89b936d137d4a433bbb570cc536e8d8
Kang <61106285+flipped-1121@users.noreply.github.com>
2021-02-05 16:24:58 +0800

    Update cheat.js

--- cheat.js
+++ cheat.js
@@ -2,12 +2,12 @@
  var titleTime;
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
-         $('[rel="icon"]').attr('href', "/img/favicon.ico");
+         $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/flipped-1121/BlogPictures/flipped-1121-PIC/36.png");
          document.title = '(つェ⊂)我藏好了哦~';
          clearTimeout(titleTime);
      } 
      else {
-         $('[rel="icon"]').attr('href', "/img/favicon.ico");
+         $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/flipped-1121/BlogPictures/flipped-1121-PIC/36.png");
          document.title = '(*´∇｀*) 被你发现啦~' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
